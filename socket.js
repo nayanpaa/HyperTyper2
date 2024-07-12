61435c2665e5212aa912122ecd4ad9b699491231
Nayan Paavalar <nayanpaa@umich.edu>
2024-07-12 17:01:05 -0500

    added event listeners

--- socket.js
+++ socket.js
@@ -1,8 +1,13 @@
+const rooms = new Map()
+
 function handleSocket(io) {
   io.on('connection', (socket) => {
     console.log('New client connected:', socket.id);
 
-    socket.on('createRoom', (roomId) => {
+    socket.on('createRoom', (roomId, playerName) => {
+      const newRoom = { players: new Map(), gameStarted: false };
+      newRoom.players.set(socket.id, {name: playerName, progress: 0 }); 
+      rooms.set(roomId, newRoom);
       socket.join(roomId);
       io.to(roomId).emit('roomCreated', roomId);
       console.log(`Room created: ${roomId}`);
@@ -14,11 +19,37 @@ function handleSocket(io) {
       console.log(`Player ${socket.id} joined room: ${roomId}`);
     });
 
-    socket.on('gameUpdate', (data) => {
-      io.to(data.roomId).emit('gameUpdate', data);
+    socket.on('startGame', (roomId) => {
+      const room = rooms.get(roomId);
+      if (room && room.players.size > 1) {
+        room.gameStarted = true;
+        io.to(roomId).emit('gameStarted');
+      }
     });
 
+    socket.on('updateMyProgress', (roomId, progress) => {
+      const room = rooms.get(roomId);
+      if (room && room.players.has(socket.id)) {
+        room.players.get(socket.id).progress = progress;
+        io.to(roomId).emit('progressUpdate', {
+          playerId: socket.id,
+          progress: progress
+        });
+      }
+    });
+
+    //add end game listener
+
     socket.on('disconnect', () => {
+      rooms.forEach((room, roomId) => {
+        if (room.players.has(socket.id)) {
+          room.players.delete(socket.id);
+          io.to(roomId).emit('playerLeft', socket.id);
+          if (room.players.size === 0) {
+            rooms.delete(roomId);
+          }
+        }
+      });
       console.log('Client disconnected:', socket.id);
     });
   });
